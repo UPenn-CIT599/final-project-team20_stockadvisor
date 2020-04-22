2af0f6db6a600de031bcf528881f9f0aa59a889f
melvin <huli@seas.upenn.edu>
2020-04-22 13:03:45 -0400

    a

--- StockAdvisor/src/main/java/com/java/YahooFinanceAPI/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.java.YahooFinanceAPI;
-
-import static spark.Spark.*;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
-}
